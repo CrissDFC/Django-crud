b1df2c3a7e050d5fc48394559468e7d961ae491e
Cristian Fonseca <cridafoca1@gmail.com>
2025-03-19 14:10:52 -0500

    actualiazcion de setttings y entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo "→ Aplicando migraciones..."
-python manage.py migrate
-
 echo "→ Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
 
-echo "→ Iniciando servidor Gunicorn..."
-gunicorn django_crud.wsgi:application --bind 0.0.0.0:8000
+
+echo "→ Aplicando migraciones..."
+python manage.py migrate
+
+
+
+# Inicia Gunicorn
+exec gunicorn --bind 0.0.0.0:8000 django_crud.wsgi:application
